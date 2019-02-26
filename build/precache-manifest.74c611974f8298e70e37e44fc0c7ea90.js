28c95da9272165f4d3635b8e61cf8188a3571412
Grace Gude <grace@Graces-MBP.localdomain>
2019-02-26 15:10:15 -0700

    build

---
+++ build/precache-manifest.74c611974f8298e70e37e44fc0c7ea90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3e2dda20620991514d7e1d2f77c37e6",
+    "url": "/static/media/bnwbluff.f3e2dda2.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c330aa9c8df96992da3d",
+    "url": "/static/js/main.c330aa9c.chunk.js"
+  },
+  {
+    "revision": "e1f9bf5ecf5de4935be3",
+    "url": "/static/js/1.e1f9bf5e.chunk.js"
+  },
+  {
+    "revision": "c330aa9c8df96992da3d",
+    "url": "/static/css/main.93d5e740.chunk.css"
+  },
+  {
+    "revision": "e1f9bf5ecf5de4935be3",
+    "url": "/static/css/1.ef05086d.chunk.css"
+  },
+  {
+    "revision": "fe8b027a3cee64a38313e427d9c8dfb7",
+    "url": "/index.html"
+  }
+];
